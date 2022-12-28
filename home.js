aa050b815bc19df4a198803149aac7bda4c47fc4
Pappu Dey <pappudey.coder@gmail.com>
2022-12-28 15:28:00 +0600

    simple calculator

---
+++ home.js
@@ -0,0 +1,35 @@
+/*
+console.log("total ", 5 + 5, 7 * 7);
+
+      alert("hello World");
+      var , let , const
+
+      var name = "pappu dey";
+      name = "Swarna ";
+      var roll = 18;
+
+      var name = "swarna ", roll = 18;
+      var name = "Sourov";
+
+      let name = "Nasifa";
+      name = "Pappu Dey";
+
+      const name = "Anas";
+       name = "Saad";
+      console.log(name);
+
+      */
+
+
+// let number1 = 30;
+// let number2 = 20;
+
+
+// let $ = 10;
+// let _ = 20
+
+let firstName = "pappu";
+let lastName = "Dey";
+
+// console.log(firstName+ lastName);
+document.write(firstName + " " + lastName)
