346cea1a121df29bb410d8ee23e66d66dfb0f1b1
Pappu Dey <pappudey.coder@gmail.com>
2023-01-12 14:50:22 +0600

    js day 07 last update

--- main.js
+++ main.js
@@ -1,50 +1,65 @@
-// {
-//     const myName = 'Pappu'
-       
+
+let userString = "It's ,development , development"
+// console.log(userString.length)
+// console.log(userString.slice(11))
+
+// console.log(userString.substring(2, 10))
+
+// console.log(userString.substr(2, 13))
+
+// console.log(userString.replace("development", "design"))
+
+// console.log(userString.replaceAll())
+
+
+// console.log(userString.toUpperCase())
+// console.log(userString.toLowerCase())
+
+// let textOne = "Hello";
+// let textTwo = "World"
+
+// let result = textOne.concat(" ", textTwo)
+// console.log(result)
+
+// let userName = " dfg   pappu      ";
+// console.log(userName.trim());
+// console.log(userName.trimStart());
+// console.log(userName.trimEnd())
+
+
+
+// let ourText = "Ban| gla desh";
+// console.log(ourText.split(" |"))
+
+
+// let userDetails = `Lorem ipsum dolor ${2+2} sit amet consectetur.`
+// console.log(userDetails)
+
+// let userDetails = `Lorem ipsum dolor sit amet consectetur. Lorem sit`;
+
+// console.log(userDetails.indexOf("Lorem"))
+// console.log(userDetails.lastIndexOf("Lorem"))
+
+// console.log(userDetails.indexOf("sit", ))
+
+
+
+// console.log(userDetails.search("lorem"))
+
+// let userDataCon = userDetails.toLowerCase();
+
+// if(userDataCon === "lorem"){
+//     console.log("true")
 // }
 
-// console.log(myName)
+
+// let userDetails = ` ipsum dolor sit amet consectetur.  sit  Lorem`;
+
+// console.log(userDetails.match("ipsum"))
 
 
-// myName = "pappu";
+// console.log(userDetails.includes("Lorem", 8))
 
-// let myName
-// console.log(myName)
-
-
-
-
-// function myCal(p1, p2 , p3){
-
-//     let numOne = p1;
-//     let numTwo = p2;
-//     let call = p3
-
-//     if (call == "+"){
-//         result = numOne + numTwo;
-//         console.log(result)
-//     }
-//     else if (call == "-"){
-//         result = numOne - numTwo;
-//         console.log(result)
-//     }else if(call == "/") {
-//         result = numOne / numTwo;
-//         console.log(result)
-//     }
-//     else if(call == "*") {
-//         result = numOne * numTwo;
-//         console.log(result)
-//     }
-//     else{
-//         console.log('Error')
-//     }   
-
-// }
-
-// myCal(10,20, "-");
-// myCal(10,20, "+")
-// myCal(20, 10, "/");
-// myCal(20, 2, "*")
-
+// console.log(userDetails.endsWith("Lorem"))
 
 
