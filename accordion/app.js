7ce390aff5a3dbea08981376637269fe649e9270
Pappu Dey <pappudey.coder@gmail.com>
2023-03-23 13:33:31 +0600

    js clss 17 all code

--- accordion/app.js
+++ accordion/app.js
@@ -1,18 +1,31 @@
 
 
 function addClass(){
-    let showText = document.querySelector(".text_hide");
-    let addBtn = document.querySelector(".add_Btn");
-    let removeBtn = document.querySelector(".remove_Btn");
+    let hideText = document.querySelector(".text_hide");
+    let ctaBtn = document.querySelector(".ctaBtn");
+    let toggleBtn = document.querySelector(".toogleBtn");
+   
+    hideText.classList.toggle("text_show")
+    ctaBtn.classList.toggle("rotateBtn")
+    console.log( )
+    // let btnTxt = toggleBtn.innerHTML;
+    
 
+    // if(btnTxt === "Add"){
+    //     toggleBtn.innerHTML = "Remove "
+        
+    // }else{
+    //     toggleBtn.innerHTML = "Add "
+    // }
+    // console.log(btnTxt)
 
     // paragrap text text show class add 
-    showText.classList.add("text_show")
+    // hideText.classList.add("text_show")
 
     // add button display none 
-    addBtn.classList.add("btn_add_show")
+    // addBtn.classList.add("btn_add_show")
 
-    removeBtn.classList.add("remove_btn_show")
+    // removeBtn.classList.add("remove_btn_show")
    
 }
 
